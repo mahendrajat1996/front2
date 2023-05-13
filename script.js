3c938a8fae3b1fd9f90224a5e8fdf955db3d42a8
mahendrajat1996 <130475100+mahendrajat1996@users.noreply.github.com>
2023-05-13 22:33:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,71 @@
 /** @format */
 
 let arr = [
-  { id: 1, name: "john", age: "18", marks: 80 },
-  { id: 2, name: "jack", age: "20", marks: 85 },
-  { id: 3, name: "karen", age: "19", marks: 35 },
-];
-
-function PrintStudentswithMap() {
-  //Write your code here , just console.log
-}
-
-function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
-}
-
-function addData() {
-  //Write your code here, just console.log
-}
-
-function removeFailedStudent() {
-  //Write your code here, just console.log
-}
-
-function concatenateArray() {
-  //Write your code here, just console.log
+    { id: 1, name: "john", age: "18", marks: 80 },
+    { id: 2, name: "jack", age: "20", marks: 85 },
+    { id: 3, name: "karen", age: "19", marks: 35 },
+  ];
+  
+  //Student With graterThen 50 marks with map function
+  let output = arr.map(PrintStudentswithMap);
+  function PrintStudentswithMap(element) {
+    if (element.marks > 50) {
+      return element.name;
+    }
+  }
+  console.log(output);
+  
+  // PrintStudentsbyForEach///////
+  let namess = [];
+  arr.forEach(PrintStudentsbyForEach);
+  
+  function PrintStudentsbyForEach(element) {
+    if (element.marks > 50) {
+      namess.push(element.name);
+    }
+  }
+  console.log(namess);
+  
+  //addData///////
+  function addData() {
+    console.log(arr);
+  }
+  let obj = { id: 4, name: "suman", age: "23", marks: 45 };
+  arr.push(obj);
+  console.log(arr);
+  
+  //Remove Failed Student
+  let filterdata = arr.filter(removeFailedStudent);
+  function removeFailedStudent(elemtt) {
+    if (elemtt.marks > 50) return arr.splice();
+  }
+  console.log(filterdata);
+  
+  // New Array with New Objects
+  function concatenateArray() {
+    let student = [
+      {
+        id: 11,
+        name: "Radha",
+        age: "29",
+        friends: ["mohan", "gopi", "raghav"],
+        city: "Kota",
+      },
+      {
+        id: 11,
+        name: "Radha",
+        age: "21",
+        friends: ["mohini", "priya", "ginni"],
+        city: "Delhi",
+      },
+      {
+        id: 11,
+        name: "Radha",
+        age: "18",
+        friends: ["suman", "puja", "poonam"],
+        city: "Bangloeru",
+      },
+    ];
+    console.log(student);
+  }
 }
